43ace38c7cdc61f16f3b304afa6d624180c073da
Your Name <you@example.com>
2022-07-30 00:32:36 -0300

    Sas y seo

---
+++ css/style.cs
@@ -0,0 +1,37 @@
+@charset "UTF-8";
+/*VARIABLES*/
+/*colores*/
+/*espaciados*/
+/*FUENTES*/
+/*TAMAÑOS DE FUENTE*/
+/*MIXINS*/
+/*PORTIALS*/
+header {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: align;
+  padding: 1.2em;
+  border-bottom: 2px solid red;
+}
+header .nav-item {
+  max-width: 120px;
+}
+header .menu {
+  display: flex;
+  gap: 1.2em;
+}
+header .nav-item {
+  font-size: x-small;
+  padding: auto;
+}
+header .nav-item:hover {
+  color: red;
+}
+
+body {
+  background-color: rgb(107, 88, 70);
+  color: rgb(245, 241, 241);
+}
+
+/*# sourceMappingURL=style.cs.map */
